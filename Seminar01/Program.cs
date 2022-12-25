e5b251da1fc4c0d84e3ebab00b64f11326d68533
vzhiv <vzhiv@fvis.ru>
2022-12-25 21:53:02 +0300

    Add data for seminar01

--- Seminar01/Program.cs
+++ Seminar01/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Семинар 1.
+
+//Задача 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a first number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = num2 * num2;
+
+if(num1 == quad2)
+{
+    Console.WriteLine("Yes!");
+}
+else
+{
+    Console.WriteLine("No!");
+}
+*/
+
+// Задача 2. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+/*
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = -number;
+
+while(current <= number)
+{
+    Console.Write(current + " ");
+    current = current + 1; //current += 1; current++
+}
+*/
+
+
+/*
+37145 / 10 = 3714
+37145 / 100 = 371
+37145 / 1000 = 37
+37145 / 10000 = 3
+37145 / 100000 = 0
+
+37145 % 10 = 5
+37145 % 100 = 45
+37145 % 1000 = 145
+37145 % 10000 = 7145
+*/
