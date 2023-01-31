95ee18625bacd7f6827ed7111827d64a873ba204
vzhiv <vzhiv@fvis.ru>
2023-01-31 03:48:56 +0300

    add new data homework05

--- HomeWork05/Program.cs
+++ HomeWork05/Program.cs
@@ -93,6 +93,15 @@ Console.Write("Сумма элементов массива стоящих на 
         array[i] = Math.Round(minValue + new Random().NextDouble()*(maxValue - minValue), 2); // Искуственно сокращаю вещественное число до 2-х знаков после запятой
     }
     return array;
+} */
+
+/* double[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    double[] array = new double[size];
+    for(int i = 0; i < size; i++)
+        array[i] = Math.Round((new Random().Next(minValue, maxValue) + new Random().NextDouble()), 2);
+     
+    return array;
 }
 
 void ShowArray(double[] array)
@@ -124,11 +133,52 @@ Console.Clear();
 Console.Write("Введите число элементов массива: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите минимальное значение массива: ");
-double min = Convert.ToDouble(Console.ReadLine());
+int min = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите максимальное значение массива: ");
-double max = Convert.ToDouble(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
 
 double[] newArray = CreateRandomArray(m, min, max);
 ShowArray(newArray);
 double result = Math.Round(DiffMaxMin(newArray), 2); // Искуственно сокращаю вещественное число до 2-х знаков после запятой
 Console.Write("Разница между максимальным и минимальным элементами массива: " + result); */
+
+
+double[] CreateRandomDoubleArray(int size, int minValue, int maxValue)
+{
+    double[] array = new double[size];
+    for(int i = 0; i < size; i++)
+        array[i] = Math.Round((new Random().Next(minValue, maxValue) + new Random().NextDouble()), 2);
+     
+    return array;
+}
+
+void ShowArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+double GetDiff(double[] array)
+{
+    double min = array[0];
+    for(int i = 1; i < array.Length; i++)
+        if(array[i] < min) min = array[i];
+    double max = array[0];
+    for(int i = 1; i < array.Length; i++)
+        if(array[i] > max) max = array[i];
+    double result = Math.Round(max - min, 2);
+    return result;
+}
+
+Console.WriteLine("Введите количество элементов массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимально-возможное значение элемента массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимально-возможное значение элемента массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+double[] array = CreateRandomDoubleArray(length, min, max);
+ShowArray(array);
+
+Console.WriteLine("Разница между максимальным и минимальным элементом массива = " + GetDiff(array));
